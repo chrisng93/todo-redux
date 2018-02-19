4d5d8deea44dc1a514da775524f9af58382fe2b2
Chris Ng <chrisng93@gmail.com>
2018-02-19 16:30:45 -0500

    add logger

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,6 +1,15 @@
-import {createStore} from 'redux';
+import {applyMiddleware, createStore} from 'redux';
+import {createLogger} from 'redux-logger';
 import todoReducer from './todoReducer';
 
+const logger = createLogger({
+  collapsed: true,
+  duration: true,
+  diff: true,
+});
+
+const createStoreWithMiddleware = applyMiddleware(logger)(createStore);
+
 export default function configureStore(initialState) {
-  return createStore(todoReducer, initialState);
+  return createStoreWithMiddleware(todoReducer, initialState);
 }
